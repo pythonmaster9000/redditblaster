99e3eba352f1a86a89315c06ad2339ebe44e0d87
pythonmaster9000 <bigswaggydude10@gmail.com>
2023-08-31 21:05:18 -0400

    comment initial

---
+++ cli.py
@@ -0,0 +1,2 @@
+import organizer.py
+
---
+++ utils/comment.py
@@ -0,0 +1,56 @@
+import tlssession
+import json
+
+
+class AccountError(Exception):
+    """Account not in tokens.json"""
+    pass
+
+
+class commentHelper:
+    def __init__(self, account_name, comment):
+        with open(r'..\tokens.json', 'r') as f:
+            account_info = json.load(f)
+        try:
+            self.account = account_info[account_name]
+        except KeyError:
+            raise AccountError()
+        self.recaptcha_token = self.account['recaptcha_token']
+        self.authorization = self.account['authorization']
+        self.x_reddit_loid = self.account['x-reddit-loid']
+        self.x_reddit_session = self.account['x-reddit-session']
+        # TODO what the fck is thing_id
+        self.payload = {
+            'api_type': 'json',
+            'return_rtjson': 'true',
+            'thing_id': 't3_166fmbt',
+            'recaptcha_token': self.recaptcha_token,
+            'text': comment,
+            'richtext_json': {"document": [{"e": "par", "c": [{"e": "text", "t": comment}]}]}
+        }
+        self.headers = {
+            "accept": "*/*",
+            "accept-language": "en-US,en;q=0.9",
+            "authorization": self.authorization,
+            "content-type": "application/x-www-form-urlencoded",
+            "sec-ch-ua": "\"Chromium\";v=\"116\", \"Not)A;Brand\";v=\"24\", \"Google Chrome\";v=\"116\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-site",
+            "x-reddit-loid": self.x_reddit_loid,
+            "x-reddit-session": self.x_reddit_session,
+            "Referer": "https://www.reddit.com/",
+            "Referrer-Policy": "origin-when-cross-origin"
+        }
+        self.url = 'https://oauth.reddit.com/api/comment.json?rtj=only&emotes_as_images=true&redditWebClient=desktop2x&app=desktop2x-client-production&raw_json=1&gilding_detail=1'
+        self.session = tlssession.session()
+
+    def post_comment(self):
+        res = self.session.post(
+            self.url,
+            headers=self.headers,
+            data=self.payload
+        )
+        return res
